1a1ae5ce9c82da0297bd6d7e7366f1e6bc7bb882
cchebelok <lotokhinivan@gmail.com>
2022-06-18 23:05:33 +0300

    made a window for game, create file with constants

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("Hello world!")
-prin("second commit test")
+from tkinter import *
+import random
+import constants as c
+
+root = Tk()
+root.title('Snake Game')
+root.resizable(False, False)
+
+score = 0
+label =Label(root, text="Score:{}".format(score), font=('caliblri', 20))
+label.pack(side=TOP, anchor=NW)
+
+canvas = Canvas(root, width=c.WIDTH, height=c.HEIGHT, bg=c.BG_COLOR)
+canvas.pack()
+
+root.mainloop()
