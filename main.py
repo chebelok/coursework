b005351835bf20f69fb62ffb3ae112fcef549bde
cchebelok <lotokhinivan@gmail.com>
2022-06-18 23:55:10 +0300

    added Food class

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from tkinter import *
 import random
 import constants as c
 
+class Food:
+    def __init__(self):
+        x = random.randint(0, 22) * c.PART_SIZE
+        y = random.randint(0, 22) * c.PART_SIZE
+        self.coordinate = [x, y]
+        canvas.create_oval(x, y, (x + c.PART_SIZE), (y + c.PART_SIZE), fill=c.FOOD_COLOR)
+
 root = Tk()
 root.title('Snake Game')
 root.resizable(False, False)
@@ -13,4 +20,5 @@ label.pack(side=TOP, anchor=NW)
 canvas = Canvas(root, width=c.WIDTH, height=c.HEIGHT, bg=c.BG_COLOR)
 canvas.pack()
 
+food = Food()
 root.mainloop()
