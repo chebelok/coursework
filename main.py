1eff5c973b314b5ab7bd6d0bcb023d1de776d244
cchebelok <lotokhinivan@gmail.com>
2022-06-19 22:20:20 +0300

    rewrite all, using oop, add image to background

--- main.py
+++ main.py
@@ -2,23 +2,27 @@ from tkinter import *
 import random
 import constants as c
 
-class Food:
-    def __init__(self):
-        x = random.randint(0, 22) * c.PART_SIZE
-        y = random.randint(0, 22) * c.PART_SIZE
-        self.coordinate = [x, y]
-        canvas.create_oval(x, y, (x + c.PART_SIZE), (y + c.PART_SIZE), fill=c.FOOD_COLOR)
-
 root = Tk()
 root.title('Snake Game')
 root.resizable(False, False)
 
-score = 0
-label =Label(root, text="Score:{}".format(score), font=('caliblri', 20))
-label.pack(side=TOP, anchor=NW)
+class Window(Canvas):
+    def __init__(self):
+        super().__init__(width=c.WIDTH, height=c.HEIGHT, highlightthickness=0, bg=c.BG_COLOR)
+        self.bg = PhotoImage(file='grass.png')
+        self.create_image(0, 0, image=self.bg, anchor="nw")
+        self.pack()
+        self.score = 0
+        self.label = Label(root, text=f"Score:{self.score}", font=('calibri', 20))
+        self.label.pack(side=TOP, anchor="nw")
+        self.create_food()
 
-canvas = Canvas(root, width=c.WIDTH, height=c.HEIGHT, bg=c.BG_COLOR)
-canvas.pack()
+    def create_food(self):
+        x = random.randint(0, 22) * c.PART_SIZE
+        y = random.randint(0, 22) * c.PART_SIZE
+        self.coordinate = [x, y]
+        self.create_oval(x, y, (x + c.PART_SIZE), (y + c.PART_SIZE), fill=c.FOOD_COLOR)
 
-food = Food()
+
+win = Window()
 root.mainloop()
