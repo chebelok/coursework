baca1c1473ecf1a66fc2e2efa5d6b68b48431698
cchebelok <lotokhinivan@gmail.com>
2022-06-20 02:32:38 +0300

    add some class methods

--- main.py
+++ main.py
@@ -6,23 +6,40 @@ root = Tk()
 root.title('Snake Game')
 root.resizable(False, False)
 
-class Window(Canvas):
+
+class Main(Canvas):
     def __init__(self):
         super().__init__(width=c.WIDTH, height=c.HEIGHT, highlightthickness=0, bg=c.BG_COLOR)
         self.bg = PhotoImage(file='grass.png')
-        self.create_image(0, 0, image=self.bg, anchor="nw")
-        self.pack()
+        self.coordinate = []
+        self.x = c.START_X
+        self.y = c.START_Y
+        self.dir = c.move_way
+        self.start_dir = self.dir['down']
         self.score = 0
+        self.create_image(0, 0, image=self.bg, anchor="nw")
+        self.create_food()
+        self.scoreboard()
+        self.snake_part(self.x, self.y)
+        self.pack()
+
+    def scoreboard(self):
         self.label = Label(root, text=f"Score:{self.score}", font=('calibri', 20))
         self.label.pack(side=TOP, anchor="nw")
-        self.create_food()
+
+    def update_score(self):
+        self.score = self.score+1
+        self.label['text'] = "Score:{}".format(self.score)
 
     def create_food(self):
         x = random.randint(0, 22) * c.PART_SIZE
         y = random.randint(0, 22) * c.PART_SIZE
         self.coordinate = [x, y]
-        self.create_oval(x, y, (x + c.PART_SIZE), (y + c.PART_SIZE), fill=c.FOOD_COLOR)
+        self.create_oval(x, y, (x + c.PART_SIZE), (y + c.PART_SIZE), fill=c.FOOD_COLOR, tags='food')
+
+    def snake_part(self, x, y):
+        self.part = self.create_rectangle(x, y, x + c.PART_SIZE, y + c.PART_SIZE, fill=c.SNAKE_COLOR)
 
 
-win = Window()
+win = Main()
 root.mainloop()
